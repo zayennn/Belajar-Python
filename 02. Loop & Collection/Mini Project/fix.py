7f2c3102e60e2483e4cf0c5b0ce1dc8390466264
zayennn <zaayeennn@gmail.com>
2026-03-22 11:22:26 +0700

    update

---
+++ 02. Loop & Collection/Mini Project/fix.py
@@ -0,0 +1,30 @@
+data_siswa = []
+
+jumlah_siswa = int(input("masukan jumlah siswa : "))
+
+total_nilai = 0
+
+for i in range(jumlah_siswa) :
+    print(f"================ input siswa ke - {i} ================\n")
+    nama = input("masukan nama siswa : ")
+    nilai = int(input("masukan nilai siswa : "))
+    
+    siswa = {
+        "nama" : nama,
+        "nilai" : nilai
+    }
+    
+    data_siswa.append(siswa)
+    total_nilai += nilai
+    
+nilai_rata_rata = total_nilai / jumlah_siswa
+
+print("\n================ DAFTAR SISWA ================")
+for siswa in data_siswa :
+    print(f"""
+===========================
+nama    : {siswa["nama"]}
+nilai   : {siswa["nilai"]}
+===========================""")
+
+print("nilai rata rata siswa adalah : ", nilai_rata_rata)
