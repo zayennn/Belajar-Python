e0a353c508f848f789095977e8a741a7614081c1
zayennn <zaayeennn@gmail.com>
2026-03-22 09:19:42 +0700

    update

--- 01. Fundamental/2. Variable/app.py
+++ 01. Fundamental/2. Variable/app.py
@@ -2,8 +2,8 @@ nama = "elang"
 umur = 19
 tinggi = 175.5
 
-# nama → nama variabel
-# = → assignment (ngasih nilai)
-# "Elang" → string
-# 18 → integer
-# 170.5 → float
+# nama      → nama variabel
+# =         → assignment (ngasih nilai)
+# "Elang"   → string
+# 18        → integer
+# 170.5     → float
