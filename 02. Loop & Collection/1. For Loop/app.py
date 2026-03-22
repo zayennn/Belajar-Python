2e15ad23e31f035c03d3bd100458e48461481906
zayennn <zaayeennn@gmail.com>
2026-03-22 10:08:20 +0700

    update

--- 02. Loop & Collection/1. For Loop/app.py
+++ 02. Loop & Collection/1. For Loop/app.py
@@ -1,6 +1,6 @@
 for i in range(5) :
     print(i)
     
-# for → perulangan
-# i → variabel loop
-# range(5) → dari 0 sampai 4
+# for       → perulangan
+# i         → variabel loop
+# range(5)  → dari 0 sampai 4
