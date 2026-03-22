9128d1286a0fdfd2c081f0bf661ad11276bb0f45
zayennn <zaayeennn@gmail.com>
2026-03-22 10:52:11 +0700

    update

--- 02. Loop & Collection/Mini Project/app.py
+++ 02. Loop & Collection/Mini Project/app.py
@@ -4,11 +4,11 @@ nilai_siswa = []
 jumlah_siswa = int(input("masukan jumlah siswa : "))
 
 for i in range(jumlah_siswa) :
-    nama = input(f"masukan nama siswa ke - {i} : ")
-    nilai = int(input(f"masukan nilai siswa ke - {i} : "))
+    nama = input(f"masukan nama siswa ke - {i + 1} : ")
+    nilai = int(input(f"masukan nilai siswa ke - {i + 1} : "))
     
-    nama_siswa += nama[i]
-    nilai_siswa += nilai[i]
+    nama_siswa[i] += nama
+    nilai_siswa[i] += nilai
     
 print("================ DAFTAR SISWA ================")
 for i in range(jumlah_siswa) :
