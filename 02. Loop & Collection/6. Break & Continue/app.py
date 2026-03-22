24b48e6366a3c6f6784caa5ccfd6fe76009614db
zayennn <zaayeennn@gmail.com>
2026-03-22 10:34:16 +0700

    update

--- 02. Loop & Collection/6. Break & Continue/app.py
+++ 02. Loop & Collection/6. Break & Continue/app.py
@@ -6,8 +6,10 @@ for i in range(10) :
     
 # → berhenti di 5
 
-for i in range(10) :
-    if i % 2 == 0 :
+for i in range(1, 10 + 1) :
+    if i % 2 == 1 :
         continue
     
     print(i)
+
+# → skip angka ganjil
