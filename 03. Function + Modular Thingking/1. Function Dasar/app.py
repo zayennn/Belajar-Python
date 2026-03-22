a6ed21f14a044477fa468dd6785426a15457cde1
zayennn <zaayeennn@gmail.com>
2026-03-22 11:55:20 +0700

    update

--- 03. Function + Modular Thingking/1. Function Dasar/app.py
+++ 03. Function + Modular Thingking/1. Function Dasar/app.py
@@ -1,2 +1,11 @@
 def sapa() :
     print("hello world")
+    
+# def   → keyword buat bikin function
+# sapa  → nama function
+# ()    → parameter (kosong)
+# :     → wajib
+# function ga jalan kalau ga dipanggil
+
+# cara manggil :
+sapa()
