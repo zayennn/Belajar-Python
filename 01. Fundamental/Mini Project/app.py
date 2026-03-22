047446f0121109f5291247dee3bc6c941543c457
zayennn <zaayeennn@gmail.com>
2026-03-22 09:58:44 +0700

    update

--- 01. Fundamental/Mini Project/app.py
+++ 01. Fundamental/Mini Project/app.py
@@ -1,3 +1,22 @@
 nama = input("masukan nama anda : ")
 umur = int(input("masukan umur anda : "))
-status = "anak anak" if umur <= 13 else "remaja" if umur <= 17 else "dewasa"
+status = ""
+
+if umur < 13 :
+    print("""
+nama    : {nama}
+umur    : {umur}
+status  : anak anak
+        """)
+elif umur <= 17 :
+        print("""
+nama    : {nama}
+umur    : {umur}
+status  : remaja
+        """)
+else : 
+        print("""
+nama    : {nama}
+umur    : {umur}
+status  : dewasa
+        """)
